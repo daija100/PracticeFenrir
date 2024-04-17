86c65298288e4691f9e83f47e0267bb5ac478d8c
Daija Canegan <daijac@vandenrobotics.com>
2024-04-17 16:55:18 -0700

    Added to subsystem and made follower motors

--- Astrid-2024/src/main/java/frc/robot/Constants.java
+++ Astrid-2024/src/main/java/frc/robot/Constants.java
@@ -5,15 +5,31 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final boolean kOpposeMasterDirection = true;
+  public static final boolean kDontOpposeMasterDirection = false;
+  
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static class ChassisConstants {
+
+    public static final int kDriveFrontLeft = 1;
+    public static final int kDriveBackLeft = 2;
+    public static final int kDriveFrontRight = 3;
+    public static final int kDriveBackRight = 4;
+
   }
 }
--- Astrid-2024/src/main/java/frc/robot/subsystems/Chassis.java
+++ Astrid-2024/src/main/java/frc/robot/subsystems/Chassis.java
@@ -4,14 +4,36 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Chassis extends SubsystemBase {
+  private TalonFX DriveFrontLeft;
+  private TalonFX DriveFrontRight;
+  private TalonFX DriveBackLeft;
+  private TalonFX DriveBackRight;
+
   /** Creates a new Chassis. */
-  public Chassis() {}
+  public Chassis() {
+    DriveFrontLeft = new TalonFX(Constants.ChassisConstants.kDriveFrontLeft);
+    DriveFrontRight = new TalonFX(Constants.ChassisConstants.kDriveFrontRight);
+    DriveBackLeft = new TalonFX(Constants.ChassisConstants.kDriveBackLeft);
+    DriveBackRight = new TalonFX(Constants.ChassisConstants.kDriveBackRight);
+
+    DriveFrontLeft.setControl(
+        new Follower(DriveBackLeft.getDeviceID(), Constants.kDontOpposeMasterDirection));
+
+          DriveFrontRight.setControl(
+     new Follower(DriveBackRight.getDeviceID(), Constants.kDontOpposeMasterDirection));
+
+  }
 
   @Override
   public void periodic() {
+   
     // This method will be called once per scheduler run
   }
 }
